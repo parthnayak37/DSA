0c4bd8d46f6b552795a04926aaa38571cc98b240
parthnayak37 <98223294+parthnayak37@users.noreply.github.com>
2023-10-01 11:35:42 +0530

    Create printNumber.cpp

---
+++ Questions/functions/printNumber.cpp
@@ -0,0 +1,14 @@
+//week 2 lecture 3 function intro
+
+#include<iostream>
+using namespace std;
+
+void printNumber(int num){
+  cout<<num<<endl;
+}
+int main(){
+  int a=5;
+printNumber(a);
+  }
+ 
+
